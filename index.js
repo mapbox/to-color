8b3f64282987ec7d6079ddfd6e8e3bed522f5dc2
Tom MacWright <tom@macwright.org>
2015-07-30 13:19:20 -0400

    Tolerate non-string input

--- index.js
+++ index.js
@@ -6,18 +6,22 @@
  * @returns {string} output color
  * @example
  * toColor('tom') //= 'rgba(187,153,68,0.75)'
+ * toColor() //= 'rgba(0,0,0,0.74)'
  */
 function toColor(str, opacity) {
   var rgb = [0, 0, 0, opacity || 0.75];
-  for (var i = 0; i < str.length; i++) {
-    var v = str.charCodeAt(i);
-    var idx = v % 3;
-    rgb[idx] = (rgb[i % 3] + (13 * (v % 13))) % 12;
-  }
+  try {
+    for (var i = 0; i < str.length; i++) {
+      var v = str.charCodeAt(i);
+      var idx = v % 3;
+      rgb[idx] = (rgb[i % 3] + (13 * (v % 13))) % 12;
+    }
+  } finally {
   return 'rgba(' +
     rgb.map(function(c, idx) {
       return idx === 3 ? c : (4 + c) * 17;
     }).join(',') + ')';
+  }
 }
 
 module.exports = toColor;
