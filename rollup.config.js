c395cc344e80f65a6ddd1a654ad50a0caeebb8c6
tristen <tristen.brown@gmail.com>
2021-02-01 23:12:57 -0500

    Set up for publisher

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  input: ['index.js'],
+  input: ['src/index.js'],
   output: [
     {
       name: 'toColor',
       file: 'dist/to-color.js',
+      format: 'umd'
+    }, {
+      name: 'toColor',
+      file: 'demo/to-color.js',
       format: 'umd',
       sourcemap: true
     }
